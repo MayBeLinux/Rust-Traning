24f5183e69ef83d7e49e7454875a7575ea715c56
Josh <159950906+MayBeLinux@users.noreply.github.com>
2025-09-27 23:06:31 +0200

    add_hello_world

---
+++ hello_world/src/main.rs
@@ -0,0 +1,16 @@
+fn main() { // déclaration de la fonction principale nommée main
+    println!("Hello, world!"); // Println avec "!" comme caractère spécial est utilisé pour les macros / sinon c'est une fonction normale
+} // utilisation des accolades pour délimiter le bloc de code de la fonction
+
+
+// INFO:
+
+// Rust est sensible à la casse, donc "main" et "Main" seraient considérés comme deux identificateurs différents.
+
+// Chaque instruction se termine par un point-virgule ";"
+
+// Les commentaires commencent par "//" et s'étendent jusqu'à la fin de la ligne.
+
+// Les blocs de code sont délimités par des accolades "{}".
+
+// Les chaînes de caractères sont délimitées par des guillemets doubles "".
