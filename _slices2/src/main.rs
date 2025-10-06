3e5859665406321c6fe3704bf52ce42c269dc73b
Josh <159950906+MayBeLinux@users.noreply.github.com>
2025-10-06 23:10:15 +0200

    update slice2

--- _slices2/src/main.rs
+++ _slices2/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let slice = &a[0..3];
-    println!("{:?}", slice);
+    println!(" Nous somme dans la fonction main {:?}", slice);
     let sum = print_slice(&a);
 }
 
 
 fn print_slice(sliced : &[i32] ) {
     let lenght = sliced.len();
-    println!("Length of the slice is {}", lenght);
+    println!("Nous somme dans la fonction print slice {}", lenght);
     
 }
